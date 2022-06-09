13b209699999bc8d009e9e20010e29735f8a5e97
Alrusso42 <alrussoprivate@hotmail.com>
2022-06-09 07:45:58 +0200

    y

--- Exam Rank 02/Level 2/Corrections/ft_atoi.c
+++ Exam Rank 02/Level 2/Corrections/ft_atoi.c
@@ -1,23 +1,36 @@
-int     ft_atoi(const char *str)
+int ft_atoi(const char *str)
 {
-    int     i;
-    int     result;
-    int    neg;
+	int	i;
+	int	neg;
+	int result;
 
-    i = 0;  
-    result = 0;
-    neg = 1;
-    while (str[i] == '\n' || str[i] == '\t' || str[i] == '\v' || str[i] == '\r' || str[i] == '\f')
-        i++;
-    if (str[i] == '-')
-        neg *= -1;
-    else if (str[i] == '+')
-        i++;
-    while(str[i] != '\0' && str[i] >= '0' && str[i] <= '9')
-    {
-        result *= 10;
-        result += str[i] - '0';
-        i++;
-    }
-    return (result * neg);
+	i = 0;
+	result = 0;
+	neg = 1;
+	while(((str[i] >= 9 && str[i] <= 13) || str[i] == 32) && str[i])
+		i++;
+	if(str[i] == '-')
+	{
+		neg = -1;
+		i++;
+	}
+	else if(str[i] == '+')
+		i++;
+	while(str[i] != '\0' && (str[i] >= '0' && str[i] <= '9'))
+	{
+		result *= 10;
+		result += str[i] - '0';
+		i++;
+	}
+	return(result * neg);
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int		main(void)
+{
+	printf("%d\n", ft_atoi("123456789"));
+	printf("%d\n", atoi("123456789"));
+	return (0);
 }
